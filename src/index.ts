97b731a0d32452ed1ce26b16f4c25616b9c7c372
DilshodJS <dilshoddev6969@gmail.com>
2022-03-14 18:56:33 +0430

    sada

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+// 5 -----------------------------------------
+
+// let arr: number[] = [67,101]
+// let x: number = 66
+// console.log(arr.includes(x));
+
+
+// 2 -----------------------------------------
+
+
+// let n: number  = 121432
+// if(n >= 0){
+//     let m: number = n - n - n
+//     console.log(m);
+// }
+// else{
+//     console.log(n);    
+// }
+
+
+// 4 ------------------------------------------
+
+
+// let x: string = 'dsadsas'
+// let y: number = x.length / 2
+// if(x.length % 2 == 1){ 
+//     console.log(x.substring(y-0.5, y+0.5)); 
+// }
+// else{
+//     console.log(x.substring(y-1,y+1));
+// }
+
+
+// 3 --------------------------------------------
+
+
+// let a: string = '348597'
+// let c: string[] | number[] = []
+// for(let i = 0; i < a.length;i++){
+//     let q = +a[i]
+//     c.push(q)  
+// }
+// console.log(c);
+
+
+// 1 ---------------------------------------------
+
+
+// let m: int = 16.002
+
+// if(Math.sqrt(m) {
+
+// }
+
+
+// console.log();
